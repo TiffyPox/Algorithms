7314e641cd4a9449a21f434dcc2eade9b28e1efa
TiffyPox <tiffanyjaynedavies@hotmail.co.uk>
2024-02-27 21:43:28 -0500

    update callStack

--- src/callStack.cpp
+++ src/callStack.cpp
@@ -1,21 +1,21 @@
 #include <iostream>
 
-void bye()
+void Bye()
 {
-    std::cout << "Okay bye!" << std::endl; // step 7 - function gets popped off the stack, return to greet()
+    std::cout << "Okay bye!" << std::endl; // step 7 - function gets popped off the stack, return to Greet()
 }
 
-void greet2(std::string name)
+void Greet2(std::string name)
 {
-    std::cout << "How are you " << name << "?" << std::endl; // step 4 - function gets popped off the stack, return to greet()
+    std::cout << "How are you " << name << "?" << std::endl; // step 4 - function gets popped off the stack, return to Greet()
 }
 
-void greet(std::string name)
+void Greet(std::string name)
 {
     std::cout << "Hello " << name << "!" << std::endl; // step 2 - print greeting
-    greet2(name); // step 3 - greet2() function call added on top of the stack, print question
+    Greet2(name); // step 3 - Greet2() function added to top of the stack, print question
     std::cout << "Time to say goodbye..." << std::endl; // step 5 - getting ready to say goodbye
-    bye(); // step 6 - bye() function call added on top of the stack
+    Bye(); // step 6 - Bye() function added to top of the stack
 
     return; // step 8 - nothing else to be done
 }
@@ -24,7 +24,7 @@ int main()
 {
     std::string name = "Tiffany";
 
-    greet(name); // step 1 - stack created, call greet()
+    Greet(name); // step 1 - stack created, call Greet()
 
     return 0;
 }
