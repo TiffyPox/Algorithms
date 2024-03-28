687f21b04a73f43a3fb4751a8780068efbe4d5ba
TiffyPox <tiffanyjaynedavies@hotmail.co.uk>
2024-03-27 20:52:51 -0400

    update hashTables

--- src/hashTables.cpp
+++ src/hashTables.cpp
@@ -8,11 +8,11 @@ int main()
 
     std::map<std::string, float> groceries; // hash table
 
-    groceries["Apelsin Jus"] = 0.67;
+    groceries["Apelsin Jus"] = 0.67; // key + value
     groceries["Mjolk"] = 1.49;
     groceries["Kaffe"] = 1.99;
 
-    std::cout << groceries["Mjolk"] << std::endl;
+    std::cout << groceries["Mjolk"] << std::endl; // prints value of key
 
     std::map<std::string, int> phoneBook;
 
@@ -20,7 +20,7 @@ int main()
     phoneBook["Asterix"] = 786547892;
     phoneBook["Obelix"] = 111234543;
 
-    std::cout << phoneBook["Asterix"];
+    std::cout << phoneBook["Asterix"] << std::endl;
 
     return 0;
 }
