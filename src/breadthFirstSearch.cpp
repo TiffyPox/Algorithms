e5091692dbea3834d2c5129cc1b0ff6c75516a65
TiffyPox <tiffanyjaynedavies@hotmail.co.uk>
2024-09-18 22:29:19 +0100

    update breadthFirstSearch

--- src/breadthFirstSearch.cpp
+++ src/breadthFirstSearch.cpp
@@ -1,41 +1,88 @@
 #include <iostream>
-#include <map>
+#include <cstdlib>
 #include <string>
+#include <vector>
+#include <map>
 #include <queue>
 
-void printQueue(std::queue<std::map<std::string, std::string>> q) 
+bool personIsSeller(const std::string& name)
 {
-    while (!q.empty()) 
-    {
-        const auto& map = q.front();
-        for (const auto& pair : map) 
-        {
-            std::cout << pair.first << ": " << pair.second << ", ";
-        }
-
-        std::cout << std::endl;
-
-        q.pop();
-    }
+	// name ending in 'm' means mango seller.. why not?
+	return !name.empty() && name.back() == 'm';
 }
 
-
-int main() 
+void printQueue(std::queue<std::map<std::string, std::vector<std::string>>> q) 
 {
-    std::map<std::string, std::string> graph;
-    
-    graph["You"] = "Kirk";
-    graph["Kirk"] = "Scott";
-    graph["Spock"] = "Sulu";
-    graph["McCoy"] = "Chekov";
+	while (!q.empty()) 
+	{
+		const auto& map = q.front();
+		for (const auto& pair : map) 
+		{
+			std::cout << "\n" << pair.first << ": ";
+			
+			for (const auto& name: pair.second)
+			{
+				std::cout << name << " ";
+			}
+		}
 
-    std::queue<std::map<std::string, std::string>> searchQueue;
+		std::cout << std::endl;
 
-    searchQueue.push(graph);
+		q.pop();
+	}
+}
 
-    printQueue(searchQueue);
+int main()
+{
+	std::cout << "Looking for a mango seller..." << std::endl;
 
-    // To Do: Change the order that elements are printed. 
+    std::map<std::string, std::vector<std::string>> graph;
+
+	graph["You"].push_back("Alice");
+	graph["You"].push_back("Bob");
+	graph["You"].push_back("Claire");
+	graph["Bob"].push_back("Anuj");
+	graph["Bob"].push_back("Peggy");
+	graph["Alice"].push_back("Peggy");
+	graph["Claire"].push_back("Thom");
+	graph["Claire"].push_back("Jonny");
+	graph["Anuj"] = {};
+	graph["Peggy"] = {};
+	graph["Thom"] = {};
+	graph["Jonny"] = {};
+
+	std::queue<std::string> searchQueue;
+
+	std::map<std::string, bool> searched;
+
+	searchQueue.push("You");
+
+	while (!searchQueue.empty())
+	{
+		std::string person = searchQueue.front();
+		searchQueue.pop();
+		
+		if (searched[person] == false)
+		{
+			if (personIsSeller(person))
+			{
+				std::cout << person << " is a mango seller!" << std::endl;
+
+				return 0;
+			}
+			else
+			{
+				for (const auto&neighbour : graph[person])
+				{
+					searchQueue.push(neighbour);
+				}
+
+				searched[person] = true;
+			}
+		}
+	}
+
+	std::cout << "No mango seller was found" << std::endl;
 
     return 0;
 }
